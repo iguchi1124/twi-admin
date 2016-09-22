0495a87d0e10b9f31c680060139a8f68a8fae3dd
Shota Iguchi <shota-iguchi@cookpad.com>
2016-09-22 23:25:20 +0900

    Add refresh cached users feature

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,41 +2,45 @@ class Account < ApplicationRecord
   validates :provider, inclusion: { in: ['twitter'] }
 
   def follower_ids
-    @follower_ids ||= Rails.cache.fetch(cache_key(:follower_ids)) {
+    @follower_ids ||= Rails.cache.fetch(cache_key(:follower_ids)) do
       rest_client.follower_ids.to_a
-    }
+    end
   end
 
   def followers
-    @followers ||= Rails.cache.fetch(cache_key(:followers)) {
+    @followers ||= Rails.cache.fetch(cache_key(:followers)) do
       users(follower_ids)
-    }
+    end
   end
 
   def friend_ids
-    @friend_ids ||= Rails.cache.fetch(cache_key(:friend_ids)) {
+    @friend_ids ||= Rails.cache.fetch(cache_key(:friend_ids)) do
       rest_client.friend_ids.to_a
-    }
+    end
   end
 
   def friends
-    @friends ||= Rails.cache.fetch(cache_key(:friends)) {
+    @friends ||= Rails.cache.fetch(cache_key(:friends)) do
       users(friend_ids)
-    }
+    end
   end
 
   def unreturned_friends
-    @unreturned_friends ||= Rails.cache.fetch(cache_key(:unreturned_friends)) {
+    @unreturned_friends ||= Rails.cache.fetch(cache_key(:unreturned_friends)) do
       users(friend_ids - follower_ids)
-    }
+    end
   end
 
   def follow!(*user_ids)
-    rest_client.follow(users(user_ids.flatten))
+    users_to_follow = users(user_ids.flatten)
+    rest_client.follow(users_to_follow)
+    refresh_cached_friends(friends + users_to_follow)
   end
 
   def unfollow!(*user_ids)
-    rest_client.unfollow(users(user_ids.flatten))
+    users_to_unfollow = users(user_ids.flatten)
+    rest_client.unfollow(users_to_unfollow)
+    refresh_cached_friends(friends - users_to_unfollow)
   end
 
   def followed?(user)
@@ -92,5 +96,15 @@ class Account < ApplicationRecord
     end
 
     ids_users.values
+  end
+
+  def refresh_cached_friends(friends)
+    Rails.cache.write(cache_key(:friend_ids), friends.map(&:id))
+    Rails.cache.write(cache_key(:friends), friends)
+  end
+
+  def refresh_cached_followers(followers)
+    Rails.cache.write(cache_key(:follower_ids), followers.map(&:id))
+    Rails.cache.write(cache_key(:followers), followers)
   end
 end
